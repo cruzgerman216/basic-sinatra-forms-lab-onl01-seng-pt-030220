465031f0f073446c951835eadc080bbe51dc67b3
German Cruz <cruzgerman216@yahoo.com>
2020-05-13 02:29:19 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,6 +3,13 @@ require 'sinatra/base'
 class App < Sinatra::Base
 
   get "/newteam" do
+    @team = params[:name]
+    @coach = params[:coach]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @sf = params[:sf]
+    @pf = params[:pf]
+    @c = params[:c]
     erb :newteam
   end
 
