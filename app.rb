ef762b16bb213d491382d76d85a2afe414ecbd86
German Cruz <cruzgerman216@yahoo.com>
2020-05-13 02:48:25 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,19 +2,20 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
-  get "/newteam" do
-
+  get '/newteam' do
     erb :newteam
   end
 
-  post "/team" do
-    @team = params[:name]
-    @coach = params[:coach]
-    @pg = params[:pg]
-    @sg = params[:sg]
-    @sf = params[:sf]
-    @pf = params[:pf]
-    @c = params[:c]
+  post '/team' do
+    @name = params["name"]
+    @coach = params["coach"]
+    @pg = params["pg"]
+    @sg = params["sg"]
+    @sf = params["sf"]
+    @pf = params["pf"]
+    @center = params["c"]
+
     erb :team
   end
+
 end
